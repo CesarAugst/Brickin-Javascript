422d6aae4a8e2e6c3e53d4efe736e54e28a7e0f4
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2020-05-13 00:33:47 -0300

    nome e idade
    Agora ele armazena em local storage o nome e a idade, e disponibiliza pela pagina, na situação atual podemos usar o nome digitado pelo usuario

--- codigo/js/script.js
+++ codigo/js/script.js
@@ -1,7 +1,22 @@
 function comecar(){
+
+    //pega os id e coloca em variaveis
     var nome = document.getElementById("inputNome");
     var idade = document.getElementById("inputIdade");
 
-    localStorage.setItem("nome", nome.value)
-    localStorage.setItem("idade", idade.value)
+    //guarda o que foi digitado e coloca na localStorage
+    localStorage.setItem('nome', nome.value);
+    localStorage.setItem('idade', idade.value);
+    
+    //redireciona o usuario
+    location.href = "menuEstudos.html";
+}
+
+function bemVindo(){
+    //pega o id 
+    var titulo = document.getElementById("h1Titulo");
+    //pega o nome
+    var nome = localStorage.getItem('nome');
+
+    titulo.innerHTML += ' ' +nome.toUpperCase();
 }
